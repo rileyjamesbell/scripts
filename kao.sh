171cb94f8b92717e9fcffbf7c50d2032147ae583
rileyjamesbell <bellriley5@gmail.com>
2020-02-02 18:58:41 -0800

    Update kaomoji script

--- kao.sh
+++ kao.sh
@@ -4,8 +4,9 @@ menu="rofi -dmenu"
 kaofile=$(dirname $0)/kao.txt
 
 kao=$(cat $kaofile|$menu)
+[ "$kao" != "" ] || exit
 echo $kao|xclip -selection clipboard
 echo $kao|xclip -selection primary
 echo $kao|xclip -selection secondary
 echo $kao|xclip -selection buffer-cut
-notify-send "$(xclip -o)"
+notify-send "'$(xclip -o)' copied to clipboard."
